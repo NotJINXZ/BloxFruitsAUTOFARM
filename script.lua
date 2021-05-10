c368c2f7987c6cf4c1a85c3052761d1a2a44521e
JINXZ <58348317+NotJINXZ@users.noreply.github.com>
2021-05-10 17:13:43 -0400

    Added Script (C&P)

---
+++ script.lua
@@ -0,0 +1,19 @@
+--[[
+Made by Not JINXZ#9925
+Discord: dsc.gg/jinxz
+
+Enjoy!
+
+Themes:
+    LightTheme
+    DarkTheme
+    GrapeTheme
+    BloodTheme
+    Ocean
+    Midnight
+    Sentinel
+    Synapse
+]]--
+
+getgenv().ThemeOption = "Midnight"  -- Themes are listed above!
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/BloxFruitsNew/main/src.lua"))()
